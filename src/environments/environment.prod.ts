a334ceedf24d898c8a1e34ced554d02b84a206ff
Darko Fotinovski <fotinovskidarko@gmail.com>
2021-01-07 00:57:00 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCcgWvostpMO4Jtb7Wj_3LQajVC4BEIW1Q",
+    authDomain: "to-do-app-88805.firebaseapp.com",
+    databaseURL: "https://to-do-app-88805-default-rtdb.firebaseio.com",
+    projectId: "to-do-app-88805",
+    storageBucket: "to-do-app-88805.appspot.com",
+    messagingSenderId: "693235839104",
+    appId: "1:693235839104:web:3588a2138d23e1bd13b665",
+  },
 };
